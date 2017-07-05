f53b55e8eb4b353e58de00add16384bdfc06a8f3
Eric C. Weig <eweig@email.uky.edu>
2017-07-05 14:48:27 -0400

    Create fallback.php

---
+++ fallback.php
@@ -0,0 +1,173 @@
+<?php
+# Legacy NKAA Pattern: /record.php?note_id=3035
+# Omeka Pattern: /nkaa/items/show/3035
+
+if (preg_match('/^\/record.php\?note\_id\=(.*)/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $id = $output_array[1];
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/items/show/";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path$id";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+
+# Legacy NKAA Pattern: /source.php?source_id=165
+# Omeka Pattern: /nkaa/items/show/30000165
+
+    elseif (preg_match('/^\/source.php\?source\_id\=(.*)/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $id = $output_array[1];
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/items/show/";
+    $redirectCode = "301";
+    $gap = "30000";
+    $newURL = "$protocal$domain$path$gap$id";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /sources.php?sort_by=Z
+# Omeka Pattern: /nkaa/references/nkaa-sources#Z
+
+    elseif (preg_match('/^\/sources.php\?sort\_by\=(.*)/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $id = $output_array[1];
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/references/nkaa-sources#";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path$id";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /all.php?sort_by=H
+# Omeka Pattern: /nkaa/references/nkaa-items#H
+
+    elseif (preg_match('/^\/all.php\?sort\_by\=(.*)/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $id = $output_array[1];
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/references/nkaa-items#";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path$id";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /directors.php?sort_by=H
+# Omeka Pattern: /nkaa/references/nkaa-usa-library-directors#H
+
+    elseif (preg_match('/^\/directors.php\?sort\_by\=(.*)/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $id = $output_array[1];
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/references/nkaa-usa-library-directors#";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path$id";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /aboutus.php
+# Omeka Pattern: /nkaa/about
+
+    elseif (preg_match('/^\/aboutus.php/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/about";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /index.php
+# Omeka Pattern: /nkaa/
+
+    elseif (preg_match('/^\/index.php/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /directors.php
+# Omeka Pattern: /nkaa/references/nkaa-usa-library-directors
+
+elseif (preg_match('/^\/directors.php/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/references/nkaa-usa-library-directors";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /all.php
+# Omeka Pattern: /nkaa/references/nkaa-items
+
+elseif (preg_match('/^\/all.php/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/references/nkaa-items";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /recent_changes.php
+# Omeka Pattern: /nkaa/
+
+elseif (preg_match('/^\/recent\_changes.php/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /sources.php
+# Omeka Pattern: /nkaa/references/nkaa-sources
+
+elseif (preg_match('/^\/sources.php/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/references/nkaa-sources";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+# Legacy NKAA Pattern: /director_about.php
+# Omeka Pattern: /nkaa/about
+
+elseif (preg_match('/^\/director\_about.php/', $_SERVER['REQUEST_URI'], $output_array)) {
+    $protocal = "https://";
+    $domain = "ukscrc001.net";
+    $path = "/nkaa/about";
+    $redirectCode = "301";
+    $newURL = "$protocal$domain$path";
+    header('Location: '.$newURL);
+#    header('Location: ' . $newURL, true, $redirectCode);
+    }
+    
+    else {
+        $protocal = "https://";
+        $domain = "ukscrc001.net";
+        $path = "/default.html";
+        $newURL = "$protocal$domain$path";
+        header('Location: '.$newURL);
+    }
+    exit;
+    
+# Otherwise, do normal page
